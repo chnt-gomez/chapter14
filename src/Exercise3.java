549f532a7e69fb5cad4c79507b9c70d9c8fe4255
vicentem <vicente.martinezgz@gmail.com>
2019-10-16 23:32:16 -0500

    Solucion 2

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -8,7 +8,7 @@ public class Exercise3 {
      * Solo tu lo puedes detener. Implementa un algoritmo que, tras encontrar el numero de "a"s, reemplaza las
      * primeras letras del nombre con "X". Siendo el numero de X's el mismo que el numero de "a".
      *
-     * Si el numero de "a" encontrado es 0. el nombre quedara completamente tachado.
+     * Si el numero de "a" no es mayor que tres, se debera de escribir el nombre al revez
      */
 
     /**
